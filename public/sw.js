345dc39fd4445e26f96138dab7f53536e8d0b752
jorgefelipecastro-debug <jorgefelipecastro@gmail.com>
2026-08-26 01:49:07 +0200

    Use calculated metrics when official values are unavailable

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'encumbrate-v5';
+const CACHE_NAME = 'encumbrate-v6';
 const APP_SHELL = ['/manifest.webmanifest', '/icon-192.jpg', '/icon-512.jpg', '/apple-touch-icon.jpg'];
 
 self.addEventListener('install', (event) => {
