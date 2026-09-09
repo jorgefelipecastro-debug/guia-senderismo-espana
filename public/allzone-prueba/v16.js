1844077dee66f50ec0a7235df5fa323dc58145e3
jorgefelipecastro-debug <jorgefelipecastro@gmail.com>
2026-09-09 10:23:18 +0200

    Add Allzone V16 with new DAA vehicle images, larger movable sketch vehicles, faster navigation and downloadable PDFs

---
+++ public/allzone-prueba/v16.js
@@ -0,0 +1,5 @@
+/* Allzone V16 - fast navigation, new DAA vehicle art, downloadable DAA and photo PDFs. */
+(async function(){
+'use strict';
+const VERSION='V16 - VEHICULOS DAA + PDF';
+const ASSETS={Moto:'data:image/webp;base64,UklGRtg...'};
