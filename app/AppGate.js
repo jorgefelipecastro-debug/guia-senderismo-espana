62ec333ae2e8948d77cad9991a180900bc7d8162
jorgefelipecastro-debug <jorgefelipecastro@gmail.com>
2026-09-14 21:53:27 +0200

    Añadir acceso aislado para prueba E2E de navegación

---
+++ app/AppGate.js
@@ -0,0 +1,12 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import AuthGate from './AuthGate';
+
+const E2E_PUBLIC_PATHS = new Set(['/__offline-map-smoke']);
+
+export default function AppGate({ children }) {
+  const pathname = usePathname();
+  if (E2E_PUBLIC_PATHS.has(pathname)) return children;
+  return <AuthGate>{children}</AuthGate>;
+}
